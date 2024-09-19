8ec66ea860349f5fba6b7f8747434315b53f820f
juanAriel <arielvia2013@gmail.com>
2024-09-18 23:11:20 -0400

    install of package expo location.

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,20 +1,64 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import ButtonComponent from "../components/Atoms/Button";
 import { useNavigation } from "@react-navigation/native";
+import * as LocationExpo from "expo-location";
 
 const Location = () => {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation<any>();
+
+  useEffect(() => {
+    const getLocation = async () => {
+      const { status } = await LocationExpo.requestForegroundPermissionsAsync();
+      if (status === "granted") {
+        const loc = await LocationExpo.getCurrentPositionAsync({});
+        setLocation(loc.coords);
+      } else {
+        console.error("Permisos no concedidos");
+      }
+      setLoading(false);
+    };
+    getLocation();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (!location) {
+    return null;
+  }
+  //todo replace the location of the business that was registered
+  const targetLatitude = -17.3843499;
+  const targetLongitude = -66.2686799;
+
   const Exit = () => {
     navigation.navigate("Home");
   };
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: targetLatitude,
+          longitude: targetLongitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        showsUserLocation={true}
+      >
+        <Marker
+          coordinate={{ latitude: targetLatitude, longitude: targetLongitude }}
+          title="Ubicación Específica"
+        />
+        <Marker coordinate={location} title="Tu Ubicación" pinColor="blue" />
+      </MapView>
       <View style={styles.containerButtons}>
-        <ButtonComponent style={styles.button} title="exit" onPress={Exit} />
+        <ButtonComponent style={styles.button} title="Exit" onPress={Exit} />
       </View>
     </View>
   );
@@ -53,4 +97,5 @@ const styles = StyleSheet.create({
     borderColor: "#000",
   },
 });
+
 export default Location;
